f6e06f111e861dfd19622bfb54ed45bc30c76d0e
wjm0820 <1966943577@qq.com>
2019-03-07 20:04:06 +0800

    linux下第一次写c

---
+++ task1.c
@@ -0,0 +1,8 @@
+
+#include<stdio.h>
+int main(int argc,char ** argv)
+{
+	printf("hello world!\n");
+	return 0;
+	/* 大学里一般用int main(void),而int main(int argc,char**argv)这种是命令行参数所使用到的，argc是命令行参数的个数，argv是参数字符数组或字符数组指针 */
+}
